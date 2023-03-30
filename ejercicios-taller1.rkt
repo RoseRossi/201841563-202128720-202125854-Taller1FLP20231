9d2c46c2dc4e2de2419492431caf6f6ac8a42ba7
Yi0nn <105884704+Yi0nn@users.noreply.github.com>
2023-03-30 17:28:51 -0500

    Se agrego los puntos 2,3,4,6,8,11

--- ejercicios-taller1.rkt
+++ ejercicios-taller1.rkt
@@ -41,6 +41,87 @@
 ;(invert '((5 9) (10 90) (82 7)) multiplo5?)
 ;(invert '((6 9) (10 90) (82 7)) odd?)
 
+
+
+; Punto 2
+; down :
+; Proposito:
+; L -> L’ : Es un procedimiento que le añade un parentesis mas a cada elemento de una lista L
+;
+;<lista> :=()
+; := <Val><list>
+
+(define down
+  (lambda (L);define la funcion down con un parametro de entrada llamado L 
+    (cond
+      [(null? L) L];Si la lista esta vacia entonces la funcion devuelve una lista vacia
+      [else (cons (list (car L);devuelve el primer elemento de L creando una lista
+                        ) (down(cdr L)))])));Llama a la funcion drown y llama al resto de la lista 
+
+;Pruebas
+;(down ’(1 2 3))
+;(down ’((una) (buena) (idea)))
+;(down ’(un (objeto (mas)) complicado))
+;(down '(090201 101102 290803 030102))
+;(down '(Habia (una (vez)) alguien (( que )se mato)))
+
+
+
+; Punto 4
+; filter-in:
+; Proposito:
+; P x L -> 'L: Es un procedimiento el cual retorna los elementos que pertenecen a L en una lista con la condicion que cumpla con el predicado P
+; <P> ::= datatype?
+; <L> :== '()
+
+(define filter-in
+  (lambda (P L);dos parametros de entrada 
+    (if (= (length L) 0);verifica si la lista es vacia o no 
+        '();si es verdadero devuelve una lista vacia 
+        (if (P (list-ref L 0));mira si el primer elemento de la lista es verdadero para P
+            (cons (car L) (filter-in P (cdr L))); devuelve una lista con el primer elemento y el resto que sean verdadero para P 
+            (filter-in P (cdr L));llama a la lista sin contar el primer elemento 
+            )
+        )
+    )
+  )
+;Pruebas
+;(filter-in number? '(e 3 (5 2) w 1 ))
+;(filter-in symbol? '(x (z y) 123 owo))
+;(filter-in string? '(a b c "lenguajes" "de" "programacion" 100 123 (5 6 7)))
+;(filter-in number? '(a 2 (1 3) b 7))
+;(filter-in symbol? '(a (b c) 17 foo))
+;(filter-in string? '(a b u "univalle" "racket" "flp" 28 90 (1 2 3)))
+
+
+; Punto 6
+; swapper:
+; Proposito: 
+; E1 x E2 x L -> 'L: Es un procedimiento que devuelve los elementos pertenecientes a L, intercambiando E1 y E2 en una lista
+; <E1> ::= <int> | <string>
+; <E2> ::= <int> | <string>
+; <L> :== '()
+;     :== <int> <L> | <string> <L>
+
+(define swapper
+  (lambda (E1 E2 L);define la funcion con los parametros de entrada 
+  (cond [(null? L) empty];verifica si la lista esta vacia, para devolver vacio si es true 
+        [(equal? (car L) E1) (cons E2 (swapper E1 E2 (cdr L)))]; mira si el primer elemnto de la lista es igual a E1 si es true manda una lista donde E2 esta de primero
+        [(equal? (car L) E2) (cons E1 (swapper E1 E2 (cdr L)))]
+        [else (cons (car L) (swapper E1 E2 (cdr L)))];si el primer elemento no es igual ni a E1 ni a E2 
+        )
+    )
+  )
+;Pruebas
+;(swapper 'a 's '(d f g h))
+;(swapper 'q 'w '(e r () t y))
+;(swapper 'z 'x '(c v b n m z x c v))
+;(swapper 'a 'd '(a b c d))
+;(swapper 'a 'd '(a d () c d))
+;(swapper 'x 'y '(y y x y x y x x y))
+
+
+
 ;Punto 7
 ;funcion-append:
 ;Proposito:Procedimiento que concatena 2 listas.
@@ -82,6 +163,71 @@
 ;(cartesian-product '(a b c) ’(x y))
 ;(cartesian-product '(p q r) '(5 6 7))
 ;(cartesian-product '(ba) '(tir lada lazo ño nco rco))
+
+
+; Punto 8
+; mapping:
+; Proposito:
+; F x L1 x L2: Es un procedimiento que a cada elemento de L1 le aplica la funcion F y si el resultado es igual a L2 lo retorna
+; <F>::= <lambda exp>
+; <lambda exp>::= <identifier>
+;             ::= (lambda (<identifier>) <Function>)
+; <Function>::= <operator> <identifier> <int>
+; <operator>::= + | - | * | /
+; <L1>::= '()
+;     ::= <int> <L1> | <string> <L1>
+; <L2>::= '()
+;     ::= <int> <L2> | <string> <L2>
+
+
+(define mapping
+  (lambda (F L1 L2); define los parametros de un argumento y dos listas 
+    (if (or (null? L1)  (null? L2));revisa si alguna de las listas esta vacia
+        '(); si se cumple devuelve una lista vacia 
+        (if(equal? (F (car L1)) (car L2)); compara si el primer elemento aplicandole el F es igual
+            (cons (list (car L1) (car L2)) (mapping F (cdr L1) (cdr L2)))
+            (mapping F (cdr L1) (cdr L2));llama a la misma funcion sin el primer elemento de esta
+            )
+        )
+    )
+  )
+
+;Pruebas
+;(mapping (lambda (d) (* d 3)) (list 1 2 3) (list 3 9 12))
+;(mapping (lambda (d) (* d 2)) (list 1 2 3) (list 2 4 6))
+;(mapping (lambda (d) (* d 3)) (list 1 2 2) (list 2 4 6))
+;(mapping (lambda (d) (* d 2)) (list 1 2 3) (list 3 9 12))
+
+
+
+
+; Punto 11
+; zip:
+; Proposito:
+; F x L1 x L2 -> 'L: Es un procedimiento que aplica la funcion F sobre la posicion nesima de L1 y L2 y lo retorna en una lista
+; <F>::= <Binary function>
+; <L1>::= '()
+;     ::= <Int> <L>
+; <L2>::= '()
+;     ::= <Int> <L>
+(define zip
+  (lambda (F L1 L2); define dos listas y un argumento 
+    (cond; se utiliza como un if ya que este puede analizar varias condiciones 
+      [(eqv? F *)(map * L1 L2)];en estos se hace el analisis de quesi f es suma pues que se sumen y asi 
+      [(eqv? F -)(map - L1 L2)]
+      [(eqv? F +)(map + L1 L2)]
+      [else (map / L1 L2)]; esto es que si no es ninguno de los otros pues se divide xd 
+      )
+    )
+  )
+;Pruebas
+;(zip - '(123) '(100))
+;(zip + '(123 321) '(12 21))
+;(zip * '(43 2 1) '(23 5 32))
+;(zip + '(1 4) '(6 2))
+;(zip * '(11 5 6) '(10 9 8))
+
+
 
 ;Punto 12
 
